70aac92d2e37dede13584f7912c532c23d13da2d
donaldma <donald.ma@hotmail.ca>
2018-11-05 20:49:35 -0800

    add isNumeric function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export {default as isCurrency} from './utils/isCurrency'
+export {default as isNumeric} from './utils/isNumeric'
--- src/test/unit/index.spec.ts
+++ src/test/unit/index.spec.ts
@@ -1,15 +1,21 @@
 import { assert } from 'chai'
 import 'mocha'
 import { LoggerService } from '../../services/LoggerService'
-import { isCurrency } from '../..'
+import { isCurrency, isNumeric } from '../..'
 
 describe(LoggerService.newLine('functions test'), () => {
   it('provided a valid string isCurrency should return true or false', () => {
-
     const currencyTrue = isCurrency('BTC')
     const currencyFalse = isCurrency('dasdas')
     assert.equal(currencyTrue, true)
     assert.equal(currencyFalse, false)
+  })
+
+  it('provided a value isNumeric should return true or false', () => {
+    const numbericTrue = isNumeric(5)
+    const numbericFalse = isNumeric('dasdas')
+    assert.equal(numbericTrue, true)
+    assert.equal(numbericFalse, false)
   })
 
 })
